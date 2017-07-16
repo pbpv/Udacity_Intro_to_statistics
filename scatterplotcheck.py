6830a0b8147d2a4705026fe6db199cd684ea14c8
pbpv <pbpvusa@gmail.com>
2017-07-16 17:05:48 -0400

    Committing First draw
    Contains some notes on statistics.

---
+++ scatterplotcheck.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+#import plotly.plotly as ptly
+
+x_axis = [1400, 2400, 1800, 1900, 1300, 1100]
+y_axis = [98000, 168000, 126000, 133000, 91000, 77000]
+
+
+line = plt.figure()
+plt.plot(x_axis, y_axis)
